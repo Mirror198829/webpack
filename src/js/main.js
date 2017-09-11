02fdd0f5bcf18fa4ecb998d92f5737e1f00e94e0
Mirror198829 <412366756@qq.com>
2017-09-11 15:27:20 +0800

    导航条

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,20 @@ $('button').click(function(){
 	const userName="webpack";
 	alert(userName)
 })
+$('.common-topbar-nav-item').each(function(){
+	let that = $(this);
+	that.mouseover(function(){
+		let index =  $('.common-topbar-nav-item').index(that);
+		let left = that.position().left;
+		let width =  that.outerWidth();
+		$(".line").css({
+			'left':left,
+			'width':width
+		})
+	})
+});
+$('.common-topbar-nav-lst').mouseout(function(){
+	$(".line").css({
+		'width':0
+	})
+})
